4395ceec1664a04aab3a9d4ffb31f2d8e920ba9a
Sarah Rossy <sarahrossysings@gmail.com>
2021-03-03 01:28:38 +0000

    exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,20 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# models in ruby: way to define data structures
+# define models => change db when you run a migration
+# models are like cookie cutters which will fill the database accordingly
+
+# ORM: object-relational mapping
+# converting data between incompatible-type systems (Ruby vs SQL) using OOP
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+def store_count
+  puts Store.count
+end  
+
+store_count
